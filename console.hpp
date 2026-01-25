7ad87fbf277cd7659c0e344512b34c6777cd8641
Xie <391241193@qq.com>
2026-01-25 13:26:48 +0800

    console v1.3.0: Another Include and String Puls

--- console.hpp
+++ console.hpp
@@ -1,8 +1,207 @@
 #pragma once
-#include <bits/stdc++.h>
+#ifndef _GLIBCXX_NO_ASSERT
+#include <cassert>
+#endif
+#include <cctype>
+#include <cfloat>
+#include <climits>
+#include <csetjmp>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdlib>
+#if __cplusplus >= 201103L
+#include <cstdint>
+#if __cplusplus < 201703L
+#include <ciso646>
+#endif
+#endif
+#include <algorithm>
+#include <bitset>
+#include <functional>
+#include <iterator>
+#include <limits>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <typeinfo>
+#include <utility>
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <initializer_list>
+#include <ratio>
+#include <scoped_allocator>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#endif
+#if __cplusplus >= 201402L
+#endif
+#if __cplusplus >= 201703L
+#include <any>
+#include <optional>
+#include <variant>
+#include <string_view>
+#endif
+#if __cplusplus >= 202002L
+#include <bit>
+#include <compare>
+#include <concepts>
+#include <numbers>
+#include <ranges>
+#include <span>
+#include <source_location>
+#include <version>
+#if __cpp_impl_coroutine
+#include <coroutine>
+#endif
+#endif
+#if __cplusplus > 202002L
+#include <expected>
+#include <stdatomic.h>
+#endif
+#if _GLIBCXX_HOSTED
+#ifndef _GLIBCXX_NO_ASSERT
+#include <cassert>
+#endif
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cwchar>
+#include <cwctype>
+#if __cplusplus >= 201103L
+#include <cfenv>
+#include <cinttypes>
+#include <cstdint>
+#include <cuchar>
+#if __cplusplus < 201703L
+#include <ccomplex>
+#include <cstdalign>
+#include <cstdbool>
+#include <ctgmath>
+#endif
+#endif
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <codecvt>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+#if __cplusplus >= 201402L
+#include <shared_mutex>
+#endif
+#if __cplusplus >= 201703L
+#include <any>
+#include <charconv>
+#include <filesystem>
+#include <optional>
+#include <memory_resource>
+#include <variant>
+#endif
+#if __cplusplus >= 202002L
+#include <barrier>
+#include <bit>
+#include <compare>
+#include <concepts>
+#include <format>
+#include <latch>
+#include <numbers>
+#include <ranges>
+#include <span>
+#include <stop_token>
+#include <semaphore>
+#include <source_location>
+#include <syncstream>
+#include <version>
+#endif
+#if __cplusplus > 202002L
+#include <expected>
+#include <flat_map>
+#include <flat_set>
+#include <generator>
+#include <print>
+#include <spanstream>
+#include <stacktrace>
+#include <stdatomic.h>
+#include <stdfloat>
+#endif
+#if __cplusplus > 202302L
+#include <text_encoding>
+#include <stdbit.h>
+#include <stdckdint.h>
+#endif
+#endif
 
 namespace console
 {
+    template <class T>
+    std::ostream &operator<<(std::ostream &os, const std::vector<T> &vec);
+    template <class T, size_t n>
+    std::ostream &operator<<(std::ostream &os, const std::array<T, n> &ar);
+    template <class T, class U>
+    std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &p);
+    template <class K, class V>
+    std::ostream &operator<<(std::ostream &os, const std::map<K, V> &m);
+    template <class T>
+    std::ostream &operator<<(std::ostream &os, const std::set<T> &s);
+
     template <class T>
     std::ostream &operator<<(std::ostream &os, const std::vector<T> &vec)
     {
@@ -87,6 +286,10 @@ namespace console
             os = &Os;
             end = End;
             sep = Sep;
+        }
+        void operator()()
+        {
+            (*os) << end;
         }
         template <class T>
         void operator()(const T &t)
@@ -265,11 +468,66 @@ namespace console
     }
 
     template <class T>
-    auto choice(const T &container) -> decltype(container[0])
+    auto choice(const T &container) -> decltype(*std::begin(container))
     {
         if (container.empty())
-            throw std::invalid_argument(
-                "console::choice: Cannot choose from an empty container");
-        return container[randomint(0, container.size() - 1)];
+            throw std::invalid_argument("...");
+        auto it = std::begin(container);
+        std::advance(it, randomint(0, container.size() - 1));
+        return *it;
+    }
+
+    struct PartitionResult
+    {
+        std::string left;
+        std::string middle;
+        std::string right;
+        friend std::ostream &operator<<(
+            std::ostream &os, const PartitionResult &pr)
+        {
+            return os << '(' << pr.left << ", " << pr.middle
+                      << ", " << pr.right << ')';
+        }
+    };
+
+    PartitionResult partition(
+        const std::string &text, const std::string &sep)
+    {
+        size_t pos = text.find(sep);
+        if (pos == std::string::npos)
+            return PartitionResult{text, "", ""};
+        return PartitionResult{
+            text.substr(0, pos),
+            text.substr(pos, sep.size()),
+            text.substr(pos + sep.size())};
+    }
+
+    std::vector<std::string> split(
+        std::string text, const std::string &sep = " ")
+    {
+        std::vector<std::string> vec;
+        PartitionResult pr;
+        while ((pr = partition(text, sep)).right != "")
+        {
+            vec.push_back(pr.left);
+            text = pr.right;
+        }
+        vec.push_back(pr.left);
+        return vec;
+    }
+
+    std::string join(
+        const std::vector<std::string> &vec, const std::string &sep = "")
+    {
+        if (vec.empty())
+            return "";
+        std::stringstream ss;
+        auto it = vec.begin();
+        ss << *it;
+        while (++it != vec.end())
+        {
+            ss << sep << *it;
+        }
+        return ss.str();
     }
 }
